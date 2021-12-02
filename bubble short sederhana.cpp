6f8cdefb010cea28918fe08e907a827e38e48cd4
Siti Ayuni <92244072+sitiayuni@users.noreply.github.com>
2021-12-02 22:30:17 +0700

    Add files via upload

---
+++ bubble short sederhana.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int arr[5]={5,2,4,3,6};
+	bool swapped;
+	int temp;
+	int elemen = 4;
+	
+	do{
+		swapped=false;
+		for(int i=0; i<elemen; i++){
+			if(arr[i]>arr[i+1]){
+				temp = arr[i];
+				arr[i] = arr[i+1];
+				arr[i+1] = temp;
+				swapped = true;
+						
+			}
+		}
+	}while(swapped);
+	
+	for(int i=0; i<5;i++){
+		cout<<arr[i]<<" ";
+	}
+	
+	
+	
+}
